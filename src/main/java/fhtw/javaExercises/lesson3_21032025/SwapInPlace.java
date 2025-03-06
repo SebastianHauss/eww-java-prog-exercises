98d1cf742a8e5d9e82b56321ece3e71776f5bb1c
sh <hauss.sebastian@proton.me>
2025-03-06 15:25:03 +0100

    Vorläufige Lösung (kein Exception Handling)

---
+++ src/main/java/fhtw/javaExercises/lesson3_21032025/SwapInPlace.java
@@ -0,0 +1,28 @@
+package fhtw.javaExercises.lesson3_21032025;
+
+import fhtw.javaExercises.lesson1_04032025.EscapeCharacters;
+
+import java.util.Scanner;
+
+public class SwapInPlace {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Please give 2 numbers:");
+        String num = sc.nextLine();
+
+        String[] numbers = num.split(" ");
+        int num1 = Integer.parseInt(numbers[0]);
+        int num2 = Integer.parseInt(numbers[1]);
+
+        System.out.println("Before:");
+        System.out.println("a = " + num1);
+        System.out.println("b: " + num2);
+
+        System.out.println("After:");
+        System.out.println("a = " + num2);
+        System.out.println("b: " + num1);
+
+        sc.close();
+    }
+}
