88bd83d00fc599c1db4fa1abe034f23f8c95f7d8
sh <hauss.sebastian@proton.me>
2025-03-06 10:57:39 +0100

    Kommentar entfernt

--- src/main/java/fhtw/java/Gesicht.java
+++ src/main/java/fhtw/java/Gesicht.java
@@ -2,7 +2,6 @@ package fhtw.java;
 
 public class Gesicht {
     public static void main(String[] args) {
-        // nicht sicher ob das ganz richtig ist!
         System.out.println("0123456789012345");
         System.out.println("________________");
         System.out.println("\\              /");
