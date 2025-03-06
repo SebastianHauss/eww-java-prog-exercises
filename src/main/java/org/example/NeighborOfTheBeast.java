75a38113c5adfa9af9bcf53f903a874642dab8bc
sh <hauss.sebastian@proton.me>
2025-03-06 10:16:18 +0100

    improved solution

--- src/main/java/org/example/NeighborOfTheBeast.java
+++ src/main/java/org/example/NeighborOfTheBeast.java
@@ -2,11 +2,12 @@ package org.example;
 
 public class NeighborOfTheBeast {
     public static void main(String[] args) {
-        System.out.printf("the neighbor of the beast: %d!\n", 667);
-        System.out.printf("the neighbor of the beast: %X!\n", 667);
-        System.out.printf("the neighbor of the beast: %+10d!\n", 667);
-        System.out.printf("the neighbor of the beast: %-10d!\n", 667);
-        System.out.printf("the neighbor of the beast: %08d!\n", 667);
-        System.out.printf("the neighbor of the beast: %+-12d!", 667);
+        int beast = 667;
+        System.out.printf("the neighbor of the beast: %d!\n", beast);
+        System.out.printf("the neighbor of the beast: %X!\n", beast);
+        System.out.printf("the neighbor of the beast: %+10d!\n", beast);
+        System.out.printf("the neighbor of the beast: %-10d!\n", beast);
+        System.out.printf("the neighbor of the beast: %08d!\n", beast);
+        System.out.printf("the neighbor of the beast: %+-12d!", beast);
     }
 }
