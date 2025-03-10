c6caca96936507f265da71dfb0348f1bc702b53c
sh <hauss.sebastian@proton.me>
2025-03-10 13:05:56 +0100

    Geburtsdatum Exercise Lösung

--- src/main/java/fhtw/javaExercises/lesson2_10032025/Geburtsdatum.java
+++ src/main/java/fhtw/javaExercises/lesson2_10032025/Geburtsdatum.java
@@ -6,11 +6,16 @@ public class Geburtsdatum {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        System.out.print("Geburtsdatum: (TTMMJJ) ");
+        System.out.print("Geburtsdatum: ");
         int input = sc.nextInt();
 
-        int num1 = input / 10_000;
-        System.out.println(num1);
+        int num3 = input / 10_000;
+
+        int num2 = input / 100 % 100;
+
+        int num1 = input % 100;
+
+        System.out.printf("Ausgabe: %02d%02d%02d", num1, num2, num3);
 
         sc.close();
     }
