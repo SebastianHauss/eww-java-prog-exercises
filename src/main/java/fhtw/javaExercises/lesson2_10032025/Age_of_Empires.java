63b8f3b7136c0dd4fb73c445fb33cfd479e9e26f
sh <hauss.sebastian@proton.me>
2025-03-10 15:17:19 +0100

    Age_Of_Empires Lösung

--- src/main/java/fhtw/javaExercises/lesson2_10032025/Age_of_Empires.java
+++ src/main/java/fhtw/javaExercises/lesson2_10032025/Age_of_Empires.java
@@ -1,7 +1,27 @@
 package fhtw.javaExercises.lesson2_10032025;
 
+import java.util.Scanner;
+
 public class Age_of_Empires {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
+        System.out.print("Your age: ");
+        int age = sc.nextInt();
+
+        System.out.printf("%s: %2s" + "%d" + "%s", "%d", "~", age, "~\n");
+
+        System.out.printf("%s: %1s" + "%+d" + "%s", "%+d", "~", age, "~\n");
+
+        System.out.printf("%s: %2s" + "%d" + "%s", "% d", "~ ", age, "~\n");
+
+        System.out.printf("%s: %-4s" + "%d" + "%s", "%5d", "~", age, "~\n");
+
+        System.out.printf("%s: %s" + "%05d" + "%s", "%05d", "~", age, "~\n");
+
+        System.out.printf("%s: %s" + "%-5d" + "%s", "%-5d", "~", age, "~\n");
+
+        System.out.printf("%s: %2s" + "%#x" + "%s", "%#x", "~", age, "~\n");
     }
+
 }
